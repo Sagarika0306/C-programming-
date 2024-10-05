8f55d26035f507fb6d529b82b4d828a635a9a46e
Sagarika0306 <sagarikaroychowdhury454@gmail.com>
2024-10-05 22:29:39 +0530

    Add files via upload

---
+++ Avg.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,avg;
+    printf("\n Enter Three Numbers:");
+    scanf("%d%d%d",&a,&b,&c);
+    avg=(a+b+c)/3;
+    printf("\n Average is:%d",avg);
+
+}
