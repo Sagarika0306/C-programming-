1ad61d608e9c330125b852ba746a1ee52a03f345
Sagarika0306 <sagarikaroychowdhury454@gmail.com>
2024-10-05 22:28:57 +0530

    Add files via upload

---
+++ Absolute_value.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int a,f;
+    printf("\n Enter a number:");
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("\n output is:%d",a);
+    }
+    else(0>a);
+    {
+        f=(0-a);
+        printf("The output is:%d",f);
+    }
+}
