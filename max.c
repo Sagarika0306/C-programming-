ad61f4567c5f0866d2ea62bd7e6cade2a2d0fefe
Sagarika0306 <sagarikaroychowdhury454@gmail.com>
2024-10-26 21:53:16 +0530

    Add files via upload

---
+++ max.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter three number to check max:");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>=b && a>=c)
+    {
+        printf("%d ia a maximum number",a);
+    }
+    else if(b>=a && b>=c)
+    {
+        printf("%d is a maximum number",b);
+    }
+    else
+    {
+        printf("%d is a maximum number",c);
+    }
+}
